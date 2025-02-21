92bec988483832c8bbbfb34b249b0ca4819a25e5
Maria Duran <victoriadb496@gmail.com>
2025-02-21 12:13:54 -0500

    HotbarUi

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField]
+    private Image[] inventorySlots;
+    [SerializeField]
+    private Image[] inventoryItems;
+
+    [SerializeField]
+    private Color selectedColor;
+    [SerializeField]
+    private Color slotColor;
+
+    private int selected = 0;
+
+    private KeyCode[] Ikeys = { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4 };
+
+    private void Update()
+    {
+        for (int i = 0; i < Ikeys.Length; i++)
+        {
+            if (Input.GetKey(Ikeys[i]))
+            {
+                selected = i;
+            }
+        }
+    }
+    public void SelectedItem()
+    {
+        for (int i = 0; i < inventorySlots.Length; i++)
+        {
+            if (i == selected)
+            { 
+                inventorySlots[i].tintColor = selectedColor;
+            }
+            else
+            {
+                inventorySlots[i].tintColor = slotColor;
+            }
+        }
+    }
+
+    public void AddItem()
+    {
+        for (int i = 0; i < inventoryItems.Length; i++)
+        {
+            if (i == selected)
+            {
+                //if (ItemPicked(wtv))
+                //{
+                //    inventoryItems[i].sprite = ItemSprite(wtv);
+                //}
+            }
+        }
+    }
+}
