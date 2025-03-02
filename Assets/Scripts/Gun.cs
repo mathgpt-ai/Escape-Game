77c6383dd5ecb6647e2c2d6baee2c21cfc49ea69
Mathias Bergeron <mathias.bergeron05@gmail.com>
2025-03-02 14:31:23 -0500

    on peux add des trucs a l'inventare

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,14 +1,14 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Gun : MonoBehaviour,IInteractable
+public class Gun : MonoBehaviour, IInteractable
 {
     [SerializeField] private Sprite sp;
     Canvas can;
     GameObject go;
 
     private Inventory inventory;
+
     public string GetText()
     {
         return "yolo";
@@ -18,25 +18,34 @@ public class Gun : MonoBehaviour,IInteractable
     {
         if (inventory != null)
         {
-            inventory.AddItem(sp); // Example function call
-            print("jeaddSprite");
+            inventory.AddItem(sp); // Ajoute l'item à l'inventaire si y'est trouvé
+            
         }
     }
-
-    // Start is called before the first frame update
-   
-    
 
     void Start()
     {
-        inventory = FindObjectOfType<Inventory>(); // Find the Inventory script in the scene
+        // Démarre une coroutine pour chercher l'inventaire après 3 secondes
+        StartCoroutine(FindInventoryAfterDelay());
+    }
+
+    IEnumerator FindInventoryAfterDelay()
+    {
+        yield return new WaitForSeconds(3f); // Attend 3 secondes avant d’exécuter la suite
+
+        GameObject hotbar = GameObject.Find("Hotbar"); // Cherche l’objet "Hotbar" dans la scène
+        if (hotbar != null)
+        {
+            inventory = hotbar.GetComponent<Inventory>(); // Récupère le script Inventory
+        }
 
         if (inventory == null)
         {
-            Debug.LogError("Inventory script not found!");
+            Debug.LogError("Tabarnak! L'inventaire est pas trouvé dans Hotbar après l'attente!");
+        }
+        else
+        {
+            Debug.Log("Bon! L'inventaire est trouvé après 3 secondes.");
         }
     }
-
-
-
 }
--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,64 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.UI;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
     [SerializeField]
-    private Image[] inventorySlots;
+    private Image[] inventorySlots; // Les emplacements de l'inventaire (cadres visuels)
     [SerializeField]
-    private Image[] inventoryItems;
+    private Image[] inventoryItems; // Les images des objets dans l'inventaire
 
     [SerializeField]
-    private Color selectedColor;
+    private Color selectedColor; // Couleur du slot sélectionné
     [SerializeField]
-    private Color slotColor;
+    private Color slotColor; // Couleur des autres slots
 
-    private int selected = 0;
+    private int selected = 0; // Slot actuellement sélectionné
 
+    // Touches pour changer d'objet dans l'inventaire
     private KeyCode[] Ikeys = { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4 };
+
+    private void Start()
+    {
+        for(int i = 0; i < inventoryItems.Length; i++)
+        {
+            inventoryItems[i].enabled = false;
+        }
+    }
+
 
     private void Update()
     {
+        // Vérifie si le joueur appuie sur une des touches pour changer de slot
         for (int i = 0; i < Ikeys.Length; i++)
         {
-            if (Input.GetKey(Ikeys[i]))
+            if (Input.GetKeyDown(Ikeys[i])) // Change seulement à l’appui, évite le spam
             {
                 selected = i;
             }
         }
 
+        // Met à jour les couleurs des slots pour montrer lequel est sélectionné
         for (int i = 0; i < inventorySlots.Length; i++)
         {
-            if (i == selected)
-            {
-                inventorySlots[i].color = selectedColor;
-            }
-            else
-            {
-                inventorySlots[i].color = slotColor;
-            }
+            inventorySlots[i].color = (i == selected) ? selectedColor : slotColor;
         }
-
     }
+
     public void AddItem(Sprite newItem)
     {
+        // Ajoute un item dans le premier slot vide disponible
         for (int i = 0; i < inventoryItems.Length; i++)
         {
-            if (inventoryItems[i].sprite == null) // Check for an empty slot
+            if (!inventoryItems[i].enabled) // Vérifie si le slot est vide
             {
-                inventoryItems[i].sprite = newItem; // Assign the sprite
-                inventoryItems[i].enabled = true;   // Make sure it's visible
-                print("item added");
+                inventoryItems[i].sprite = newItem; // Assigne l’image au slot
+                inventoryItems[i].enabled = true;   // Active l’image pour l’afficher
+               return;
                 
             }
         }
 
-        print("item not aded");
+        
     }
 }
-
-
-
