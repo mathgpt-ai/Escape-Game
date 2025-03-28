e9f59b35bc90a94da448e025fc3155dad78b543d
KaliFatouj <felix.nahmias@icloud.com>
2025-03-28 11:10:39 -0400

    canvas

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    // j'ai utilisé un asset pour faire le Disolving mais je l'ai modifié à ma façon.
+
     public int x, y; // Coordonnées de la tuile
     private MeshRenderer meshRenderer;
     private Collider tileCollider;
@@ -22,12 +24,18 @@ public class Tile : MonoBehaviour
     {
         if (isDissolving)
         {
-            
             t += Time.deltaTime * speed;
-            float dissolveAmount = Mathf.Sin(t);
+            float dissolveAmount = Mathf.Clamp01(t); // Assure que ça reste entre 0 et 1
+
             Material[] mats = meshRenderer.materials;
             mats[0].SetFloat("_Cutoff", dissolveAmount);
             meshRenderer.materials = mats;
+
+            // Désactiver l'objet une fois la disparition complète
+            if (t >= 1.0f)
+            {
+                gameObject.SetActive(false);
+            }
         }
     }
 
@@ -37,9 +45,20 @@ public class Tile : MonoBehaviour
         {
             isDissolving = true;
             tileCollider.enabled = false;
+            Invoke("Respawn", 3.0f);
         }
     }
+    private void Respawn()
+    {
+        gameObject.SetActive(true);
+        isDissolving = false;
+        t = 0.0f; // Reset la progression
+        tileCollider.enabled = true;
 
+        Material[] mats = meshRenderer.materials;
+        mats[0].SetFloat("_Cutoff", 0);
+        meshRenderer.materials = mats;
+    }
     public void SetCoordinates(int x, int y, GridManager manager)
     {
         this.x = x;
