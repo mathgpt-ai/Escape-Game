86868962010abf91eaf07416def42e15dcc3a84b
Mathias Bergeron <mathias.bergeron05@gmail.com>
2025-04-17 19:14:34 -0400

    bigger balls

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -2,16 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Initialise : MonoBehaviour
+public class Teleport : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
         
     }
