490ed6eb82e2c91e8c4c28308dc46f32369b5532
KaliFatouj <felix.nahmias@icloud.com>
2025-04-19 10:47:31 -0400

    Revert "ajuster des balls"
    This reverts commit b03b28cc2cb44dedac8c49d031efe5556214fc69.

--- Assets/Scripts/LockedDoor.cs
+++ Assets/Scripts/LockedDoor.cs
@@ -1,21 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro; // For TextMeshProUGUI
+using UnityEngine.UI; // For accessing the timer text
 
 public class LockedDoor : MonoBehaviour
 {
     private Canvas canvas;
     public bool isLock = true;
     private Animator Animator;
-    private int dragonsRemaining = 3; // Total number of dragons
+    private int dragonsRemaining = 3; // 🔹 Nombre total de dragons
     [SerializeField] private AudioClip doorUnlocked;
-    [SerializeField] private float doorUnlockedVolume = 5f;
+    [SerializeField] private Text timerText; // Reference to the timer text (optional)
 
     void Start()
     {
         Animator = GetComponent<Animator>();
         canvas = GetComponentInChildren<Canvas>();
+
+        // If timer text wasn't assigned, try to find it
+        if (timerText == null)
+        {
+            // Try to find the timer text in the scene - assumes it's the same as in MazeGenerator
+            if (MazeGenerator.Instance != null)
+            {
+                // Use reflection to get the private timerText field from MazeGenerator
+                var field = typeof(MazeGenerator).GetField("timerText",
+                    System.Reflection.BindingFlags.NonPublic |
+                    System.Reflection.BindingFlags.Instance);
+
+                if (field != null)
+                {
+                    timerText = field.GetValue(MazeGenerator.Instance) as Text;
+                }
+            }
+        }
     }
 
     private void Update()
@@ -30,7 +48,7 @@ public class LockedDoor : MonoBehaviour
             Animator.SetTrigger("open");
     }
 
-    // Called when a dragon is defeated
+    // 🔹 Appelé quand un dragon meurt
     public void DragonDefeated()
     {
         dragonsRemaining--;
@@ -41,81 +59,27 @@ public class LockedDoor : MonoBehaviour
         }
     }
 
-    // Unlocks the door
+    // 🔹 Déverrouille la porte
     private void UnlockDoor()
     {
         isLock = false;
-        AudioSource.PlayClipAtPoint(doorUnlocked, Camera.main.transform.position, doorUnlockedVolume);
+        AudioSource.PlayClipAtPoint(doorUnlocked, transform.position, 40f);
 
         // Enable the minimap
-        if (MiniMap.Instance != null)
+        MiniMap.Instance.EnableMiniMap();
+
+        // Stop the timer when the door is unlocked
+        if (MazeGenerator.Instance != null)
         {
-            MiniMap.Instance.EnableMiniMap();
+            MazeGenerator.Instance.StopTimer();
+
+            
         }
 
-        // Stop and destroy any active timer
-        StopAllTimers();
-
-        // Find and destroy any timer text in the scene
-        DestroyTimerText();
-    }
-
-    // Find and destroy all TextMeshProUGUI components that might be timer texts
-    private void DestroyTimerText()
-    {
-        // Look for all TextMeshProUGUI components in the scene
-        TextMeshProUGUI[] allTexts = FindObjectsOfType<TextMeshProUGUI>();
-
-        foreach (TextMeshProUGUI text in allTexts)
+        // Hide the timer text
+        if (timerText != null)
         {
-            // Check if this text component might be a timer text
-            // We can look for typical timer formats like "00:00" or check for specific names
-            if (text.name.Contains("Timer") || text.name.Contains("Clock") ||
-                IsTimerFormatText(text.text))
-            {
-                // Make it invisible
-                text.gameObject.SetActive(false);
-
-                // Optional: Actually destroy the GameObject containing the text
-                // Destroy(text.gameObject);
-            }
-        }
-    }
-
-    // Helper method to check if a text appears to be in timer format (MM:SS)
-    private bool IsTimerFormatText(string text)
-    {
-        // Simple check for timer format (MM:SS)
-        if (text.Length == 5 && text[2] == ':')
-        {
-            // Check if the text consists of numbers separated by a colon
-            bool isTimer = true;
-            for (int i = 0; i < 5; i++)
-            {
-                if (i == 2) continue; // Skip the colon
-
-                if (!char.IsDigit(text[i]))
-                {
-                    isTimer = false;
-                    break;
-                }
-            }
-            return isTimer;
-        }
-        return false;
-    }
-
-    // Find and stop all DoorTimer components in the scene
-    private void StopAllTimers()
-    {
-        DoorTimer[] timers = FindObjectsOfType<DoorTimer>();
-        foreach (DoorTimer timer in timers)
-        {
-            if (timer != null)
-            {
-                // Call the method to stop and destroy the timer
-                timer.StopAndDestroyTimer();
-            }
+            timerText.gameObject.SetActive(false);
         }
     }
 }
