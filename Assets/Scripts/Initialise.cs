cd6b2410334c99164f21a8d635633085dea7421d
KaliFatouj <felix.nahmias@icloud.com>
2025-04-19 10:47:05 -0400

    Revert "bigger balls"
    This reverts commit 86868962010abf91eaf07416def42e15dcc3a84b.

--- Assets/Scripts/Initialise.cs
+++ Assets/Scripts/Initialise.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Teleport : MonoBehaviour
+public class Initialise : MonoBehaviour
 {
     // Start is called before the first frame update
-    private void OnCollisionEnter(Collision collision)
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         
     }
