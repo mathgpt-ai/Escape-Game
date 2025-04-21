56a62a330b483f5a198b38e795e0d9ed48393e4e
KaliFatouj <felix.nahmias@icloud.com>
2025-04-21 14:56:05 -0400

    endAlarm

---
+++ Assets/Scripts/EndAlarm.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndAlarm : MonoBehaviour
+{
+    GameObject lights = GameObject.FindWithTag("Lights");
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
