3770cf2109808434d6b710e79d4019fa10ce8dff
TristAssali <tristan.assali@hotmail.com>
2025-04-28 10:00:13 -0400

    AAAAAAAA

---
+++ Assets/Scripts/TPM.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport2 : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        collision.gameObject.transform.position = GameObject.Find("SpawnP").transform.position;
+
+    }
+
+}
+
---
+++ Assets/Scripts/goodTP.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportToMainMap : MonoBehaviour
+{
+    public Transform teleportDestination;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            rb.useGravity = true;
+
+            other.transform.position = teleportDestination.position;
+
+            zerograv zeroG = other.GetComponent<zerograv>();
+            ZeroGravityMovement zeroGravMvt = other.GetComponent<ZeroGravityMovement>();
+            FirstPersonController normalMove = other.GetComponent<FirstPersonController>();
+
+            if (zeroG != null)
+            {
+                zeroG.enabled = false;
+            }
+            if (zeroGravMvt != null)
+            {
+                zeroGravMvt.enabled = false;
+            }
+
+            if (normalMove != null)
+            {
+                normalMove.enabled = true;
+            }
+        }
+    }
+}
