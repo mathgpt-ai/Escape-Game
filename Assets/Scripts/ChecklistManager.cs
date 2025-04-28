6b9da526d6d30d6abb5f0a4d167ccb442d068b28
KaliFatouj <felix.nahmias@icloud.com>
2025-04-28 09:31:02 -0400

    tasks

--- Assets/Scripts/ChecklistManager.cs
+++ Assets/Scripts/ChecklistManager.cs
@@ -5,42 +5,151 @@ using TMPro;
 
 public class ChecklistManager : MonoBehaviour
 {
-    [SerializeField] 
+    //[SerializeField] 
+    //private TextMeshProUGUI[] enigmeTexts;
+
+
+    //private void Start()
+    //{
+    //    for (int i = 0; i < enigmeTexts.Length; i++)
+    //    {
+    //        if(PlayerPrefs.GetInt("Enigme_" + i, 0)== 1)
+    //        {
+    //            MarquerEnigeDone(i);
+    //        }
+    //    }
+    //}
+    //private void MarquerEnigeDone(int i)
+    //{
+    //    if(i >= 0 && i < enigmeTexts.Length)
+    //    {
+    //        enigmeTexts[i].text = "<s>" + enigmeTexts[i].text + "<s>"; //raye
+    //        enigmeTexts[i].color = Color.green;
+    //    }
+
+    //    //sauvegarder tache comme faite
+    //    PlayerPrefs.SetInt("Enigme_" + i, 1);
+    //    PlayerPrefs.Save();
+    //}
+
+    ////reinitialiser la checklist
+    //private void ResetChecklist()
+    //{
+    //    for(int i = 0;i < enigmeTexts.Length;i++)
+    //    {
+    //        PlayerPrefs.DeleteKey("Enigme_" + i);
+    //    }
+    //    PlayerPrefs.Save();
+
+    //}
+    public static ChecklistManager Instance;
+
+    [SerializeField]
     private TextMeshProUGUI[] enigmeTexts;
 
+    private int currentTaskIndex = 0;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
 
     private void Start()
     {
-        for (int i = 0; i < enigmeTexts.Length; i++)
-        {
-            if(PlayerPrefs.GetInt("Enigme_" + i, 0)== 1)
-            {
-                MarquerEnigeDone(i);
-            }
-        }
-    }
-    private void MarquerEnigeDone(int i)
-    {
-        if(i >= 0 && i < enigmeTexts.Length)
-        {
-            enigmeTexts[i].text = "<s>" + enigmeTexts[i].text + "<s>"; //raye
-            enigmeTexts[i].color = Color.green;
-        }
-        
-        //sauvegarder tache comme faite
-        PlayerPrefs.SetInt("Enigme_" + i, 1);
-        PlayerPrefs.Save();
+        // Cacher toutes les tâches sauf la première non terminée
+        currentTaskIndex = TrouverTacheActuelle();
+        MettreAJourChecklist();
     }
 
-    //reinitialiser la checklist
-    private void ResetChecklist()
+    private int TrouverTacheActuelle()
     {
-        for(int i = 0;i < enigmeTexts.Length;i++)
+        for (int i = 0; i < enigmeTexts.Length; i++)
+        {
+            if (PlayerPrefs.GetInt("Enigme_" + i, 0) == 0)
+            {
+                return i;
+            }
+        }
+        // Tout est terminé
+        return enigmeTexts.Length;
+    }
+
+    private void MettreAJourChecklist()
+    {
+        for (int i = 0; i < enigmeTexts.Length; i++)
+        {
+            enigmeTexts[i].gameObject.SetActive(i == currentTaskIndex);
+        }
+    }
+
+    public void MarquerEnigmeDone(int i)
+    {
+        if (i == currentTaskIndex && i < enigmeTexts.Length)
+        {
+            StartCoroutine(TransitionVersProchaineTache(i));
+        }
+    }
+
+    private IEnumerator TransitionVersProchaineTache(int i)
+    {
+        TextMeshProUGUI texte = enigmeTexts[i];
+        CanvasGroup group = texte.GetComponent<CanvasGroup>();
+
+        if (group == null)
+        {
+            group = texte.gameObject.AddComponent<CanvasGroup>();
+        }
+
+        // Faire un fade out
+        float duration = 0.5f;
+        float time = 0;
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            group.alpha = Mathf.Lerp(1, 0, time / duration);
+            yield return null;
+        }
+
+        group.alpha = 0;
+        texte.gameObject.SetActive(false);
+
+        // Passer à la prochaine tâche
+        currentTaskIndex++;
+
+        if (currentTaskIndex < enigmeTexts.Length)
+        {
+            TextMeshProUGUI prochainTexte = enigmeTexts[currentTaskIndex];
+            CanvasGroup prochainGroup = prochainTexte.GetComponent<CanvasGroup>();
+
+            if (prochainGroup == null)
+            {
+                prochainGroup = prochainTexte.gameObject.AddComponent<CanvasGroup>();
+            }
+
+            prochainGroup.alpha = 0;
+            prochainTexte.gameObject.SetActive(true);
+
+            // Fade in
+            time = 0;
+            while (time < duration)
+            {
+                time += Time.deltaTime;
+                prochainGroup.alpha = Mathf.Lerp(0, 1, time / duration);
+                yield return null;
+            }
+            prochainGroup.alpha = 1;
+        }
+    }
+
+    public void ResetChecklist()
+    {
+        for (int i = 0; i < enigmeTexts.Length; i++)
         {
             PlayerPrefs.DeleteKey("Enigme_" + i);
         }
         PlayerPrefs.Save();
-
     }
-
 }
