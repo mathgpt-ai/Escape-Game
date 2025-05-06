78be3b3a4eb342b1a1022335e3653b56f2661bdf
Mat-Top-G <mathias.bergeron05@gmail.com>
2025-05-05 21:11:31 -0400

    jai ajouter pour mon enigme

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -5,12 +5,27 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
-    
     private void OnCollisionEnter(Collision collision)
     {
-        
-        collision.gameObject.transform.position=GameObject.Find("SpawnPoint").transform.position;
-        
-    }
+        // Teleport the colliding object to the spawn point
+        collision.gameObject.transform.position = GameObject.Find("SpawnPoint").transform.position;
 
+        // Find the specific GameObject by name
+        GameObject spawner = GameObject.Find("rickorme");
+
+        // Activate the object if found
+        if (spawner != null)
+        {
+            spawner.SetActive(true);
+        }
+        else
+            Console.Write("yo");
+
+            // Find all GameObjects with the "delete" tag and destroy them
+            GameObject[] objectsToDelete = GameObject.FindGameObjectsWithTag("delete");
+        foreach (GameObject obj in objectsToDelete)
+        {
+            Destroy(obj);
+        }
+    }
 }
