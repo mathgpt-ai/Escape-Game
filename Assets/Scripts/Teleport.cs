241bc3a428eea88967390b06d16be99cd579c388
Mathias Bergeron <mathias.bergeron05@gmail.com>
2025-05-07 09:42:47 -0400

    jai changer une coupe daffaire

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -2,13 +2,22 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class Teleport : MonoBehaviour
 {
+    [SerializeField] private AudioClip teleportSound;
+    [SerializeField] private float volume = 1f;
+
     private void OnCollisionEnter(Collision collision)
     {
         // Teleport the colliding object to the spawn point
         collision.gameObject.transform.position = GameObject.Find("SpawnPoint").transform.position;
-        
-      
+
+        // Play the teleport sound if one is assigned
+        if (teleportSound != null)
+        {
+            // Play the sound at the player's new position
+            AudioSource.PlayClipAtPoint(teleportSound, collision.gameObject.transform.position, volume);
+        }
     }
 }
